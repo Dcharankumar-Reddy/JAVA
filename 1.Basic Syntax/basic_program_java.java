90ebeee14f4a9a4d142d72b93c169a58f97993dc
D charan kumar Reddy <145049794+Dcharankumar-Reddy@users.noreply.github.com>
2024-03-22 21:11:14 +0530

    Add files via upload

---
+++ 1.Basic Syntax/basic_program_java.java
@@ -0,0 +1,13 @@
+//  Main mehod Denotes Starting point of Exicution
+//String  arg[]= The main method takes parameters arg[]-Array of string type -String of wrapper class (Command line argument)
+//void=return type of main method
+//Class=gathering of datamembers and methods Eg pro1,school,bank,....
+//public =Modifier:To acces the method inside the class and out from other packages
+//static =if the Method is to be access with out object of the class the the method should bre qualied static
+class basic_program_java 
+{
+    public static void main(String arg[])
+    {
+        System.out.println("Good morning");
+    }
+}
